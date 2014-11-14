5cb755156182b29f523d6d969b3abd1d7b2f83e0
Henare Degan <henare.degan@gmail.com>
2014-11-14 12:41:12 +1100

    Don't blow up on first run

--- scraper.rb
+++ scraper.rb
@@ -52,7 +52,7 @@ end
 
 das.each do |record|
 
-  if ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? 
+  if (ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? rescue true)
     ScraperWiki.save_sqlite(['council_reference'], record)
   else
     puts "Skipping already saved record " + record['council_reference']
