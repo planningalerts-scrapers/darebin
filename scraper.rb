9ea85a10e7bf739821cb1a5a9f13e19bbcbab369
Matthew Landauer <mlandauer@gmail.com>
2019-06-02 01:41:03 +1000

    Small refactor

--- scraper.rb
+++ scraper.rb
@@ -4,10 +4,7 @@ ENV['MORPH_PERIOD'] ||= DateTime.now.year.to_s
 year = ENV['MORPH_PERIOD'].to_i
 puts "Getting data in year `#{year}`, changable via MORPH_PERIOD environment"
 
-scraper = EpathwayScraper::Scraper.new(
-  "https://eservices.darebin.vic.gov.au/ePathway/Production"
+scraper = EpathwayScraper.scrape_and_save(
+  "https://eservices.darebin.vic.gov.au/ePathway/Production",
+  list_type: :all_year, year: year
 )
-
-scraper.scrape(list_type: :all_year, year: year) do |record|
-  EpathwayScraper.save(record)
-end
