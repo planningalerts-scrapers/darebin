54f6d35b94170eaa854bfff4aa0f781f5c0aa1c2
Matthew Landauer <mlandauer@gmail.com>
2019-06-05 13:52:39 +1000

    Update to latest version of gem

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,3 @@
 require "epathway_scraper"
 
-scraper = EpathwayScraper.scrape_and_save(
-  "https://eservices.darebin.vic.gov.au/ePathway/Production",
-  list_type: :all_this_year, state: "VIC"
-)
+EpathwayScraper.scrape_and_save_authority(:darebin)
